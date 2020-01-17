1ddc5b0bdecc7a8556d2f0521fe8701f5398ddef
Robolink Teknoloji <55833343+RobolinkAkademi@users.noreply.github.com>
2020-01-17 13:13:15 +0300

    Update Obstructing Example.ino

--- Obstructing Example.ino
+++ Obstructing Example.ino
@@ -1,7 +1,7 @@
 #include <dmoto.h>
 dmoto obstacle;
-
-void setup() 
+float obs = 0;
+void setup()
 {
   Serial.begin(9600);
 
@@ -10,18 +10,23 @@ void setup()
 }
 
 void loop() {
-  Serial.println(obstacle.distance());
-
-
-  if (obstacle.distance() < 15)
+  Serial.print("Distance= ");
+  Serial.println(obs);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  obs = (duration / 2) / 29.1;
+  if (obs < 15)
   {
-   obstacle.Backward(150);
+    obstacle.Backward(150);
     delay(150);
-  obstacle.Right(0,150);
+    obstacle.Right(0, 150);
     delay(250);
   }
   else
   { obstacle.Forward(150);
   }
-
 }
